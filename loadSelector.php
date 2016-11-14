5cb41aacc8c7e9983aa170093c8869e01e9fb91b
Julian Locke <julianlocke@hotmail.com>
2016-11-14 15:22:11 -0500

    Update loadSelector.php

--- loadSelector.php
+++ loadSelector.php
@@ -1,6 +1,7 @@
 <?php
-	require_once('db.php');
+  require_once('db.php');
 
+  // Not using best practices, but we know what the data is
   foreach ($pdo->query('SELECT deck_id, deck_name FROM decks') as $row) {
     echo "<option value=".$row['deck_id'].">".$row['deck_name'].$row['deck_format']."</option>";
   }
